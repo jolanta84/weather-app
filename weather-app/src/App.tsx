9ec9a3a8c92c6836be8f578f10b27b9560123d96
jolanta.pisulska <jolanta.pisulska@gmail.com>
2022-05-27 19:48:49 +0200

    add SearchLocation component and OpenWeather API

--- weather-app/src/App.tsx
+++ weather-app/src/App.tsx
@@ -1,11 +1,32 @@
-
+import { useState, useEffect } from "react";
 import './App.css';
 import Header from "./components/Header/Header";
+import SearchLocation from './components/SearchLocation/SearchLocation';
+import axios from "axios";
 
 function App() {
+  const [city, setCity] = useState("");
+  // const [repoList, setRepolist] = useState([]);
+  // const [isFetching, setIsFetching] = useState(true);
+
+  const API_URL = 'https://api.openweathermap.org/data/2.5/';
+  const API_KEY = '0f49db41c8ba9fd435696694d7607902';
+
+  useEffect(() => {
+    if (city) {
+      axios
+        .get(`${API_URL}weather?q=${city}&appid=${API_KEY}&units=metric&lang=pl`)
+        .then((data) => {
+          console.log(data.data);
+          // setRepolist(data.data);
+          // setIsFetching(false);
+        });
+    }
+  }, [city]);
   return (
     <div className="App">
       <Header />
+      <SearchLocation />
     </div>
   );
 }
---
+++ weather-app/src/components/SearchLocation/SearchLocation.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const SearchLocation = () => {
+ 
+  return (<>
+      <input type="text" />
+      <button type="submit">Sprawdź</button></>
+  )
+    
+};
+export default SearchLocation;

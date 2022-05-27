39352de02254f084f8574998946a287459d9d22c
jolanta.pisulska <jolanta.pisulska@gmail.com>
2022-05-27 19:03:01 +0200

    add header, mui, axios

---
+++ weather-app/src/components/Header/Header.tsx
@@ -0,0 +1,14 @@
+import Typography from "@mui/material/Typography";
+import AppBar from "@mui/material/AppBar";
+
+const Header = () => {
+  return (
+    <AppBar>
+      <Typography variant="h1" align="left" sx={{ fontSize: 40, margin: 2 }}>
+  Weather App
+      </Typography>
+    </AppBar>
+  );
+};
+
+export default Header;
